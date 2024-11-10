65362cdd4e7eea44cbfef0c49dd0859a0a01fb61
Bahwinder Singh <bobin13arsenal@gmail.com>
2024-11-10 10:54:31 -0500

    db.cs updated

--- DB.cs
+++ DB.cs
@@ -12,7 +12,7 @@ namespace HypeMan
         {
             var username = Environment.GetEnvironmentVariable("MONGO_USERNAME");
             var password = Environment.GetEnvironmentVariable("MONGO_PASSWORD");
-            MongoClient client = new MongoClient("mongodb+srv://bobin13:4K8J276bWqmd5iBr@cluster0.wi0uk9y.mongodb.net/?retryWrites=true&w=majority");
+            MongoClient client = new MongoClient($"mongodb+srv://{username}:{password}@cluster0.wi0uk9y.mongodb.net/?retryWrites=true&w=majority");
 
             var db = client.GetDatabase(dbName);
             return db.GetCollection<T>(collectionName);
